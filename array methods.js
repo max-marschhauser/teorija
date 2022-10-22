1ff6b93a0dedfa92fc71c24903a8ea770aa33cc2
max-marschhauser <max.marschhauser@gmail.com>
2022-10-22 17:03:50 +0200

    file update

--- array methods.js
+++ array methods.js
@@ -1,23 +1,4 @@
-ARRAY METHODS
-
-- metode se mogu spajati, npr.
-let numberArray = [1, 2, 3, 4, 5];
-let returnValue = numberArray
-	.map((num) => num * 2)
-	.map((num) => toString())
-	.map((string) => "$"+string);
-
-
 //MIJENJANJE SADRŽAJA ARRAYA
-
-
-pop() --> makne zadnji element arraya --> fruits.pop();
-
-push() --> dodaje element na kraja arraya --> fruits.push("Kiwi");
-
-shift () --> makne prvi element arraya i shifta sve elemente za jedno mjesto unaprijed --> fruits.shift();
-
-unshift() --> dodaje element na prvo mjesto arraya --> fruits.unshift("Lemon");
 
 splice() --> briše dijelove arraya i dodaje nove na njihovo mjesto
 1. argument kaže s kojim elementom treba početi
@@ -46,7 +27,6 @@ po defaultu ide od početka do kraj arraya --> fruits.fill("Kiwi", 2, 4);
 
 
 //STVARANJE ARRAYA
-
 
 concat() --> spaja dva arraya u novi array, ne mijenja stare arraye --> const nesto = arr1.concat(arr2, arr3);
 
@@ -84,8 +64,6 @@ Ako se element ponavlja više puta onda vraća prvu / posljednju poziciju.
 
 fruits.indexOf('dates');
 
-incluces() --> vraća true ako se element nalazi u arrayu (za razliku   od indexOf, ovdje se može pronaći i NaN) --> fruits.includes("Mango");
-
 at() --> uzme broj i vrati element arraya koji se nalazi na toj poziciji (slično kao bracket notation), ako se postavi negativan 
 broj onda gleda pozicije od kraja --> arr.at(-1);
 
@@ -96,28 +74,6 @@ console.log(arr.flat(2)); --> expected output: [0, 1, 2, [3, 4]]
 
 
 //ARRAYI KOJI ZAHTJEVAJU FUNKCIJU
-
-
-forEach() --> pozove funkciju jednom na svaki elementa arraya, ne vraća array niti mijenja dijelove arraya
-npr.
-const numbers = [45, 4, 9, 16, 25];
-let txt = "";
-numbers.forEach(myFunction);
-
-function myFunction(value) {
-  txt += value + "<br>";
-}
-
-map() --> stvara novi array od trenutnog tako što odradi funkciju na svakom elementu arraya, ne mijenja stari array
-npr.
-const numbers1 = [45, 4, 9, 16, 25];
-const numbers2 = numbers1.map(myFunction);
-
-function myFunction(value) {
-  return value * 2;
-}
-
-flatMap() --> isto kao da se prvo napravio map(), a onda flat(), samo malo brže --> const flattened = arr.flatMap(num => num);
 
 filter() --> stvara novi array s elementima arraya koji bi na upit funkcije odgovorili s true
 const numbers = [45, 4, 9, 16, 25];
@@ -139,23 +95,6 @@ function myFunction(total, value) {
   return total + value;
 }
 
-every() --> vraća true ako svaki element arraya odgovara funkciji
-npr.
-const numbers = [45, 4, 9, 16, 25];
-let allOver18 = numbers.every(myFunction);
-
-function myFunction(value) {
-  return value > 18;
-}
-
-some() --> vraća true ako neki element arraya odgovara funkciji
-npr.
-const numbers = [45, 4, 9, 16, 25];
-let someOver18 = numbers.some(myFunction);
-
-function myFunction(value) {
-  return value > 18;
-}
 
 find() / findIndex() --> vraća vrijednost / poziciju prvog elementa koji pozitivno udovoljava funkciji
 npr.
@@ -165,11 +104,3 @@ let first = numbers.find(myFunction);
 function myFunction(value) {
   return value > 18;
 }
-
-
-//RAZNO
-
-
-Array.isArray() --> vraća true ako je objekt array --> let result = Array.isArray(fruits);
-
-valueOf() --> array vraća samog sebe, originalni array se ne mijenja --> fruits.valueOf();
