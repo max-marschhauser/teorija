2ce4b0d0382d7c7aa26f312738cbcf63645c44ab
Max Marschhauser <max.marschhauser@gmail.com>
2023-03-16 10:01:45 +0100

    mix-blend-mode effect css

--- typescript-test/src/app/App.tsx
+++ typescript-test/src/app/App.tsx
@@ -1,21 +1,9 @@
+import "./app.scss";
+
 export default function App() {
-	let randNum: number = 2;
-	randNum = 3;
-
-	let array: readonly number[] = [1, 2, 3];
-
-	console.log(array);
-
-	function addNum(x: number, y: number): number {
-		return x + y;
-	}
-
-	let message: string | number = "Error message";
-	message = 404;
-
-	function log(numOrString: number | string): void {
-		console.log(numOrString);
-	}
-
-	return <></>;
+	return (
+		<>
+			<section></section>
+		</>
+	);
 }
